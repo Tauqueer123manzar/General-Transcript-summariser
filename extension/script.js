32b1f7e3925d86b4ec0a1ac39b5390a28dbb7216
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-02-16 00:04:23 +0530

    code changed

---
+++ extension/script.js
@@ -0,0 +1,7 @@
+let btn=document.querySelector(".button");
+let output=document.querySelector("#output");
+btn.addEventListener("click",()=>{
+   output.innerHTML="Tauqueer manzar Tauqueer manzar Tauqueer manzar Tauqueer manzar Tauqueer manzar Tauqueer manzar Tauqueer manzar Tauqueer manzar";
+   
+    
+});
